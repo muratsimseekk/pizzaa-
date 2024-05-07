f2e2ce465b95ba822b5273039a9ef42e8b74aeba
Murat Simsek <muratsimseekk@gmail.com>
2024-05-07 18:49:00 +0300

    get additional items form array with ngFor method

--- src/app/components/order/content/formdetail/formdetail.component.ts
+++ src/app/components/order/content/formdetail/formdetail.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-formdetail',
   templateUrl: './formdetail.component.html',
-  styleUrls: ['./formdetail.component.css']
+  styleUrls: ['./formdetail.component.css'],
 })
 export class FormdetailComponent {
+  additional: string[] = [
+    'Pepperoni',
+    'Domates',
+    'Biber',
+    'Sosis',
+    'Misir',
+    'Sucuk',
+    'Kanada Jambonu',
+    'Salam',
+    'Ananas',
+    'Tavuk Izgara',
+    'Jalepenos',
+    'Kabak',
+    'Sogan',
+    'Sarimsak',
+  ];
 
+  constructor() {}
 }
