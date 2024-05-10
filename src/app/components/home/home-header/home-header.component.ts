e06b7af12450eb80491f728a3c834997f086c243
Murat Simsek <muratsimseekk@gmail.com>
2024-05-10 18:52:49 +0300

    navigates to pizza page from home with ACIKTIM button

--- src/app/components/home/home-header/home-header.component.ts
+++ src/app/components/home/home-header/home-header.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home-header',
   templateUrl: './home-header.component.html',
-  styleUrls: ['./home-header.component.css']
+  styleUrls: ['./home-header.component.css'],
 })
-export class HomeHeaderComponent {
-
-}
+export class HomeHeaderComponent {}
